8055482e94d13952f70e69ea1f1fbfba70425d2f
suis <suissetiawan11@gmail.com>
2026-02-02 23:00:26 +0700

    (chore): configure spring profiles for dev, test, prod

--- src/main/java/com/mini/project/financial_tracker/FinancialTrackerApplication.java
+++ src/main/java/com/mini/project/financial_tracker/FinancialTrackerApplication.java
@@ -1,6 +1,5 @@
 package com.mini.project.financial_tracker;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,16 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FinancialTrackerApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure()
-				.ignoreIfMissing()
-				.load();
-
-		dotenv.entries().forEach(entry -> {
-			if (System.getProperty(entry.getKey()) == null && System.getenv(entry.getKey()) == null) {
-				System.setProperty(entry.getKey(), entry.getValue());
-			}
-		});
-
 		SpringApplication.run(FinancialTrackerApplication.class, args);
 	}
 
